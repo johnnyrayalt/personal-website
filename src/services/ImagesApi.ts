a621880ce6d43ce56462baed584e03a5aa59a475
Johnny Ray Alt <johnnyrayalt@pop-os.localdomain>
2021-08-04 01:02:46 -0700

    add mandatory basic auth headers

--- src/services/ImagesApi.ts
+++ src/services/ImagesApi.ts
@@ -1,10 +1,14 @@
+import { HttpHeaders } from '../utils/http/HttpHeaders'
 import { IParsedImageList } from '../interfaces/IParsedImageList'
 
 class ImagesApi {
 
 	get = async (root: string, key: string, size: string): Promise<IParsedImageList | undefined> => {
 		try {
-			const res = await fetch(`http://localhost:4200/images/${root}/${key}/${size}`)
+			const res = await fetch(`http://localhost:4200/images/${root}/${key}/${size}`, {
+				headers: HttpHeaders.get()
+			})
+
 			return await res.json() as IParsedImageList
 		} catch (err) {
 			if (err instanceof Error) {
