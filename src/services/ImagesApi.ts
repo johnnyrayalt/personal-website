9964c86d8a6294ef69622558425df7645ba8971c
Johnny Ray Alt <johnnyrayalt@gmail.com>
2021-08-22 01:16:27 -0700

    feature(ImagesApi): begin to implement custom headers

--- src/services/ImagesApi.ts
+++ src/services/ImagesApi.ts
@@ -4,7 +4,15 @@ class ImagesApi {
 
 	get = async (root: string, key: string, size: string): Promise<IParsedImageList | undefined> => {
 		try {
-			const res = await fetch(`http://localhost:4200/images/${root}/${key}/${size}`)
+			const res = await fetch(`http://localhost:4200/images/${root}/${key}/${size}`, {
+				method: 'GET',
+				mode: 'cors',
+				headers: {
+					'Content-type': 'application/json',
+					'x-request-id': 'test',
+					'x-user-token': 'token'
+				}
+			})
 			return await res.json() as IParsedImageList
 		} catch (err) {
 			if (err instanceof Error) {
