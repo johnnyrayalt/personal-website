8100fe403d773cbf1f616428401b800762493fc5
Johnny Ray Alt <johnnyrayalt@pop-os.localdomain>
2022-01-13 17:39:09 -0800

    feat([project].tsx): implement check slug util

--- pages/projects/[project].tsx
+++ pages/projects/[project].tsx
@@ -1,39 +1,29 @@
 import React, { FC, useEffect, useState } from 'react';
 import MastHeader from '../../components/common/mastHeader/MastHeader';
 import Projects from '../../components/projects/Projects';
+import checkSlug from '../../utils/checkSlug';
 import ErrorPage from 'next/error';
 import { useRouter } from 'next/router';
-import c from '../../assets/constants';
+import { IMAGE_ROOTS } from '../../assets/constants';
 
 const Project: FC = (): JSX.Element => {
 	const router = useRouter();
  	const { project } = router.query;
 
-	 useEffect(() => {
-		 if (!project) return
-
-		 const getProjectRoot = async (): Promise<void> =>  {
-			 const projectRoot = project.toString();
-
-			 if (projectRoot === c.IMAGE_ROOTS.art || projectRoot === c.IMAGE_ROOTS.professional) {
-				 await setSlug({
-					 err: false,
-					 code: 200,
-					 slug: projectRoot,
-				 });
-			 } else {
-				 await setSlug({
-					 err: true,
-					 code: 404,
-					 slug: '404',
-				 });
-			 }
-
-		 };
-		 getProjectRoot();
-	 }, [project])
-
 	const [slug, setSlug] = useState<{err: boolean, code: number, slug: string}>({ err: false, code: 200, slug: ''});
+
+	useEffect(() => {
+	 if (!project) return
+
+	 const getProjectRoot = async (): Promise<void> =>  {
+		 const projectRoot = project.toString();
+
+		 checkSlug(projectRoot, [IMAGE_ROOTS.art, IMAGE_ROOTS.professional], setSlug);
+	 };
+	 getProjectRoot().then(() => {
+		 return;
+	 });
+	}, [project])
 
 	return (
 		<>
