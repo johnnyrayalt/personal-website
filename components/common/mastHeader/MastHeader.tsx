9e455177dc4a7b126cf823ac5cf07b153696fafe
Johnny Ray Alt <johnnyrayalt@gmail.com>
2022-01-13 19:40:38 -0800

    refactor(MastHeader): update routes

--- components/common/mastHeader/MastHeader.tsx
+++ components/common/mastHeader/MastHeader.tsx
@@ -1,14 +1,14 @@
 import React, { FC } from 'react';
-import Header from '../header/header';
-import Navigation from '../navigation/navigation';
+import Header from '../header/Header';
+import Navigation from '../navigation/Navigation';
 
-const MastHeader = () => {
+const MastHeader: FC = (): JSX.Element => {
 
 	return (
-		<div>
+		<>
 			<Header />
 			<Navigation />
-		</div>
+		</>
 	)
 }
 
