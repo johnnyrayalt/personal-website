66a337229e9b57d4ed6d0aeeafa80aa42687c328
Johnny Ray Alt <johnnyrayalt@gmail.com>
2022-01-19 18:01:10 -0800

    feat(ImageScroll.vue): properly bind images

--- src/components/works/ImageScroll.vue
+++ src/components/works/ImageScroll.vue
@@ -1,8 +1,8 @@
 <template>
   <div>images: {</div>
   <ul>
-    <li v-for="image in images" :key="image.id">
-      <img :src="image.url" :alt="setAltText()" />
+    <li v-for="image in images" :key="image + uuid()">
+      <img :src='image' alt=''>
     </li>
   </ul>
   <span>}</span>
@@ -11,10 +11,15 @@
 <script lang="ts">
 import { Vue } from 'vue-class-component';
 import { Prop } from 'vue-property-decorator';
+import { v4 as uuid } from 'uuid';
 
 export default class ImageScroll extends Vue {
-  @Prop() images!: { id: number; url: string }[];
+  @Prop() images!: any;
   @Prop() altText!: string;
+
+  uuid() {
+    return uuid();
+  }
 
   setAltText(): string {
     return this.altText !== 'none' ? this.altText : '';
